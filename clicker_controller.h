88e21f7037c03938139f7c22ccce884cf5fc06bc
Torgard-Mirdas <160980137+Torgard-Mirdas@users.noreply.github.com>
2024-03-24 14:22:31 +0100

    Create clicker_controller.h

---
+++ clicker_controller.h
@@ -0,0 +1,13 @@
+#ifndef CLICKER_CONTROLLER_H
+#define CLICKER_CONTROLLER_H
+
+class ClickerController {
+private:
+    ClickerModel model;
+    ClickerView view;
+
+public:
+    void run();
+};
+
+#endif // CLICKER_CONTROLLER_H
