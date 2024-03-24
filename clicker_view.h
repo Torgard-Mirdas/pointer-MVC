a52c6963e20df66d122ce81dd271cf1d1a24c46e
Torgard-Mirdas <160980137+Torgard-Mirdas@users.noreply.github.com>
2024-03-24 14:22:56 +0100

    Create clicker_view.h

---
+++ clicker_view.h
@@ -0,0 +1,11 @@
+#ifndef CLICKER_VIEW_H
+#define CLICKER_VIEW_H
+
+class ClickerView {
+public:
+    void displayPoints(int points);
+    void displayInvalidInputMessage();
+    void displayExitMessage();
+};
+
+#endif // CLICKER_VIEW_H
